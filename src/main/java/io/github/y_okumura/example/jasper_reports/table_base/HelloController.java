71195cd791278ed66182dfea417b0cfe7e3b21a8
y-okumura <y-okumura@nichiwa-system.co.jp>
2020-12-25 16:34:57 +0900

    Add sources.
    They ware leaked in 242fff7.

---
+++ src/main/java/io/github/y_okumura/example/jasper_reports/table_base/HelloController.java
@@ -0,0 +1,18 @@
+package io.github.y_okumura.example.jasper_reports.table_base;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
